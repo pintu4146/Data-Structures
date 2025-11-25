d09c9f043d946cbc08f3c5b4f167531367dae2d2
pintu4146 <pintoo.star4146@gmail.com>
2025-11-25 18:59:28 +0530

    mod

--- recursion/sum_n.py
+++ recursion/sum_n.py
@@ -1,9 +1,10 @@
 def sum_n(n: int) -> int:
     """
-    approach to head recussion
+    approach to head recussion to find the sum
     :param total_sum: can be int or None
     :param n: integer
     :return: total sum
+    
     """
     # base cases
     if n == 1:
